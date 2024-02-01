e79dd9c1b2fe2bc62636dbce680abb9ee36803e6
Alex Wellnitz <moin@wellnitz-alex.de>
2024-02-01 19:28:39 +0100

    feat: add open-iscsi for longhorn

--- modules/node_group/templates/init.sh
+++ modules/node_group/templates/init.sh
@@ -4,7 +4,13 @@ apt-get -yq update
 apt-get install -yq \
     ca-certificates \
     curl \
-    ntp
+    ntp \
+    open-iscsi \
+    findmnt \
+    grep \
+    awk \
+    blkid \
+    lsblk
 
 # k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_URL=https://${master_ipv4}:6443 K3S_TOKEN=${k3s_token} sh -s - \
