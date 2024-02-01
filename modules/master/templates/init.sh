3f2d61761644477bf9f88d78f02c4755fd6bc560
Alex Wellnitz <moin@wellnitz-alex.de>
2024-02-01 19:40:00 +0100

    fix: start iscsi daemon on startup

--- modules/master/templates/init.sh
+++ modules/master/templates/init.sh
@@ -12,6 +12,10 @@ apt-get install -yq \
     blkid \
     lsblk
 
+# Enable iscsi
+systemctl enable iscsid
+systemctl start iscsid
+
 # k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${k3s_channel} K3S_TOKEN=${k3s_token} sh -s - \
     --disable traefik \
